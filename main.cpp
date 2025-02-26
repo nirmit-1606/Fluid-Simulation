5c8d2a9da35710efa857ef1e8fe64e321f30cc71
Nirmit P <nirmitpatel16620@gmail.com>
2025-02-26 12:15:40 -0800

    fps calculation

--- main.cpp
+++ main.cpp
@@ -1104,11 +1104,12 @@ void Display()
 			glCallList(GridDL1);
 	}
 	
-	time1 = omp_get_wtime( );	// current clock time in seconds
 	
 	if (doSimulation){
 		
 		step();
+	
+		time1 = omp_get_wtime( );	// current clock time in seconds
 		
 		// displayCnt++;
 		if (displayCnt < 50)
